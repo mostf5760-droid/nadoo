d732f12193a3bd15cad5e539c83b7f28feaf1ffb
mostf5760-droid <mostf5760@gmail.com>
2026-02-07 20:53:34 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
-let cartCount = 0;
+const cartOverlay = document.getElementById("cartOverlay");
+const cartBtn = document.querySelector(".cart");
+const closeCart = document.getElementById("closeCart");
+const cartItemsBox = document.getElementById("cartItems");
+const cartTotal = document.getElementById("cartTotal");
 const cartCounter = document.getElementById("cart-count");
-const buttons = document.querySelectorAll(".product-card button");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    cartCount++;
-    cartCounter.textContent = cartCount;
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// فتح / إغلاق السلة
+cartBtn.onclick = () => cartOverlay.style.display = "flex";
+closeCart.onclick = () => cartOverlay.style.display = "none";
+
+// إضافة منتج
+document.querySelectorAll(".product-card button").forEach(button => {
+  button.addEventListener("click", (e) => {
+    const card = e.target.closest(".product-card");
+
+    const name = card.dataset.name;
+    const price = Number(card.dataset.price);
+
+    cart.push({ name, price });
     saveCart();
+    renderCart();
   });
 });
 
+
+function renderCart() {
+  cartItemsBox.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item, index) => {
+    total += item.price;
+
+    cartItemsBox.innerHTML += `
+      <div class="cart-item">
+        <p>${item.name}</p>
+        <span>${item.price} د.ل</span>
+      </div>
+    `;
+  });
+
+  cartTotal.textContent = total;
+  cartCounter.textContent = cart.length;
+}
+
 function saveCart() {
-  localStorage.setItem("cartCount", cartCount);
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-function loadCart() {
-  const saved = localStorage.getItem("cartCount");
-  if (saved) {
-    cartCount = saved;
-    cartCounter.textContent = cartCount;
-  }
-}
-
-loadCart();
+renderCart();
